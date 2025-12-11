900f83e5e8eb6503080ac3f840865dbf0292f0cf
pakin22326 <pakinounruen@gmail.com>
2025-12-12 01:06:55 +0700

    edit1

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
+    int n;
+    int evenCount = 0;
+    int oddCount = 0;
+
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> n;
+
+    while(n != 0){
+        if(n % 2 == 0){
+            evenCount++;
+        } else {
+            oddCount++;
+        }
+
+        cout << "Enter an integer: ";
+        cin >> n;
+    }
+
+    cout << "#Even numbers = " << evenCount << endl;
+    cout << "#Odd numbers = " << oddCount << endl;
+
     return 0;
 }
